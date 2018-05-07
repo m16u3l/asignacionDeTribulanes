e53b20865895b5e255fad308fb96c82d367dde06
m16u3l <m16u3l_777@hotmail.com>
2018-05-07 18:45:40 -0400

    lista de areas, profesionales y perfiles XD

--- app/Http/Controllers/ProfessionalController.php
+++ app/Http/Controllers/ProfessionalController.php
@@ -20,7 +20,7 @@ class ProfessionalController extends Controller
     public function professional_list(Request $request){
         $professionals = Professional::orderBy('professional_name')
                     ->search_by_name($request->name)
-					->paginate(10);
+                    ->paginate(10);
 		return view('professional.professional_list', compact('professionals'));
 	}
 
--- resources/views/professional/professional_list.blade.php
+++ resources/views/professional/professional_list.blade.php
@@ -1,74 +1,79 @@
-@extends('layouts.base') 
+@extends('layouts.base')
 @section('head')
 <title>Asignacion de Tribunales UMSS</title>
 @endsection
- 
+
 @section('child_css')
-<link href="{{ url('css/menu_professional.css')}}" rel="stylesheet" type="text/css">
+<link href="{{ url('css/pagination.css')}}" rel="stylesheet" type="text/css">
 @endsection
- 
+
 @section('content')
 <div class="col">
-  <h3>Lista de profesionales</h3>
-          <!-Buscador->
-          <div class="col-md-8 col text-center">
-            <form class="navbar-form pull right" action="{{ route ('lista_profesionales') }}" method="GET" role="search">
-              <div class="panel-body">
-                <div class="input-group input-group">
-                  <input type="text" class="form-control" name="name" placeholder="Buscar profesional..." aria-describedby="basic-addon2">
-                  <span class="input-group-append"><button type="submit" class="btn bg-theme-1 input-group-append">Buscar</button></span>
+
+  <div class="container" id="professional_list">
+    <div class="row">
+      <div class="offset-md-1 col-md-10">
+      <h3>Lista de profesionales</h3>
+        <!-Buscador->
+        <div class="mt-4 col-lg-8 col body-bg">
+          <form class="navbar-form pull right" action="{{ route ('lista_profesionales')}}" method="GET" role="search">
+            <div class="panel-body">
+              <div class="input-group input-group">
+                <input type="text" class="form-control" name="name" placeholder="Nombre profesional.." aria-describedby="basic-addon2">
+                <span class="input-group-append"><button type="submit" class="btn bg-theme-1 input-group-append">Buscar</button></span>
+              </div>
+            </div>
+          </form>
+        </div>
+        <!-Fin de buscador->
+        <br>
+        <div class="panel-body">
+          {{$professionals->total()}} registros |
+            pagina {{ $professionals->currentPage() }}
+            de {{ $professionals->lastPage() }}
+        </div>
+        <br>
+        @if ( empty($professionals[0]))
+        <h5 class="h5 text-center">No se encontró profesionales</h5>
+        @else @foreach($professionals as $professional)
+
+        <div class="card list-group-item-action element-bg mb-1">
+          <div class="card list-group-item-action">
+            <div class="card-header clearfix">
+              <div class="row">
+                <div class="col-lg-11" data-toggle="collapse" href="#{{$professional->professional_name}}">
+                  <h6 class="titleColor">{{$professional->professional_name}} {{$professional->professional_last_name_father}} {{$professional->professional_last_name_mother}}</h6>
                 </div>
               </div>
-            </form>
-          </div>
-          <!-Fin de buscador->
-          @if ( empty($professionals[0]))
-          <div class="col-md-12">
-            <h6>No se encontró profesionales</h6>
-          </div>
-          @else
-          <div class="tab-content">
-            <div id="area-related" class="container tab-pane active">
-              <div class="row" id="area-related-list">
-                @foreach($professionals as $professional)
-                <div class="col-md-6 area-related-professional" onclick="addProfessional(this)">
-                  <div class="card list-group-item list-group-item-action mb-3">
-
-                    <div class="col-11 text-justify">
-                      <div class="row">
-                        <div class="perfil col-12">
-                          <label class="h6 texto mb-0">Profesional:</label>
-                          <label class=" texto mb-0">{{$professional->professional_name}}  {{$professional->professional_last_name_father}} {{$professional->professional_last_name_mother}}</label><br>
-                        </div>
-                        <div class="perfil col-12">
-                          <label class="h6 texto mb-0">Carga de perfiles:</label>
-                          <label class=" texto mb-0">{{$professional->count}} perfiles</label>
-                        </div>
-                        <div class="perfil col-12 mb-0">
-                          <label class="h6 texto">Areas de interes:</label>
-                          <label class=" texto">Ninguna</label>
-                        </div>
-                      </div>
-                      <div class="col-12 text-center">
-                          <input type="hidden" name="_token" value="{{ csrf_token() }}"></input>
-                          <input type="hidden" name="professional_id" value="{{$professional->id}}">
-
-                        </form>
-                      </div>
-                    </div>
-                    <div class="col-md-1">
-                    </div>
-                  </div>
-                </div>
-                @endforeach
-              </div> @endif
             </div>
-            <div id="not-related" class="container tab-pane fade">
+
+            <div class="card-body collapse" id="{{$professional->professional_name}}">
+              <div class="row">
+                <div class="col-lg-11">
+                          <label class="h6 titleColor">Areas de interes:</label>
+                          
+            <!-- OSCAR ACA HAZ TU MAGIA-->
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        @endforeach @endif
       </div>
     </div>
   </div>
+  <br>
+  <div class="row">
+    <div class="col-md-3 col-xs-1"></div>
+    <div class="col-md-6 col-xs-10 mipaginacion">
+            {!! $professionals->render() !!}
+    </div>
+    <div class="col-md-3 col-xs-1"></div>
+  </div>
 </div>
 @endsection
- 
+
 @section('child_js')
+
 @endsection
