7cc989e97f0dc54e14feae1fd422ab8eaa437228
omarpepito <omarmamani408@gmail.com>
2018-05-14 17:22:37 +0200

    actualizacion de fechas

--- database/migrations/2018_04_22_034714_create_courts_table.php
+++ database/migrations/2018_04_22_034714_create_courts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAssignementsTable extends Migration
+class CreateCourtsTable extends Migration
 {
   public function up()
   {
