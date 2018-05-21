393ed5f094ebc8ea55c089e712abc74094516d34
omarpepito <omarmamani408@gmail.com>
2018-05-21 20:06:11 +0200

    registro de profesional en modal

---
+++ public/asset/professional/register_professional.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+  $("#name").addClass("input-text");
+  $("#last_name_father").addClass("input-text");
+  $("#last_name_mother").addClass("input-text");
+  $("#ci").addClass("input-number");
+  $("#cod_sis").addClass("input-number");
+  $("#phone").addClass("input-number");
+  $("#email").addClass("input-email");
+
+ $("#register1").on('click', function(e){
+
+  e.preventDefault();
+  console.log("omar");
+form = $("#register");
+ console.log(form);
+    $.ajax({
+      content_type: "application/json",
+      url: '/registrar_profesional',
+      type: "POST",
+      data: form.serialize(),
+      success: function(response){
+    if(response.status){
+        swal("Registro exitoso!","se registro a" + response.name, "success");
+        $('.swal2-confirm').on('click', function (event2) {
+      event2.preventDefault();
+      window.location.href = response.url;
+        })
+    }
+    else{
+        swal({
+      position: 'center',
+      type: 'error',
+      title: 'Error al registrar, campos por llenar',
+      showConfirmButton: false,
+      timer: 1500
+        });
+    }
+      }
+  });
+});
+});
