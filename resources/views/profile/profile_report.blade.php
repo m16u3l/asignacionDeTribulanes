5753678f1beda76a5d11a1069792ac37d707447d
Misael Javier Zurita Canaviri <mija_zuca@hotmail.es>
2018-05-21 21:28:35 -0400

    mejorando el reporte -_-

--- resources/views/profile/profile_report.blade.php
+++ resources/views/profile/profile_report.blade.php
@@ -3,64 +3,68 @@
   <head>
     <meta charset="utf-8">
     <title>Reporte de perfil</title>
-    <link rel="stylesheet" href="css/style_report.css"/>
+    <link rel="stylesheet" href="css/style.css" media="all" />
   </head>
-  <body>	
+  <body>
     <h1>{{ $profile->title }}</h1>
-    <div >
-      @foreach($profile->students as $key => $student)
-        <div>TESISTA: {{ $student->name }} {{ $student->last_name_father }} {{ $student->last_name_mother }}</div>  
-        <div>CARRERA: {{ $student->career }}</div>
-      @endforeach
-    </div>
+    <header>
+      
+      <div>  
+        @foreach($profile->students as $key => $student)
+          <div>TESISTA: {{ $student->name }} {{ $student->last_name_father }} {{ $student->last_name_mother }}</div>  
+          <div>CARRERA: {{ $student->career }}</div><br>
+        @endforeach
+      </div>  
 
-    <br>
-    <div>
+      <div>
         @foreach($profile->tutors as $key => $tutor)
           <div>TUTOR: {{ $tutor->name }} {{ $tutor->last_name_father }} {{ $tutor->last_name_mother }}</div>
-      @endforeach
-    </div>
+        @endforeach
+      </div> <br>
 
-    <h2>TRIBUNALES:</h2>
-    <div>
-      @foreach($profile->courts as $key => $court)
-        <div>TRIBUNAL: {{ $court->name }} {{ $court->last_name_father }} {{ $court->last_name_mother }}</div>
-      @endforeach
-    </div>
+      <div>TRIBUNALES</div> <br>
+      <div>
+        @foreach($profile->courts as $key => $court)
+          <div>TRIBUNAL: {{ $court->name }} {{ $court->last_name_father }} {{ $court->last_name_mother }}</div>
+        @endforeach
+      </div>
 
-    <h4>Fechas establecidas en cada fase de la defensa del perfil</h3>
-
-    <table style="width:100%">
-      <tr>
-        <th>Estado del Perfil</th>
-        <th>Fecha</th> 
-      </tr>
-      <tr>
-        <td>Iniciado</td>
-        <td>{{ $profile->date->initiated }}</td>
-      </tr>
-      <tr>
-        <td>Aprobado</td>
-        <td>{{ $profile->date->approved }}</td>
-      </tr>
-      <tr>
-        <td>Asigando</td>
-        <td>{{ $profile->date->assigned }}</td>
-      </tr>
-      <tr>
-        <td>Finalizado</td>
-        <td>{{ $profile->date->finalized }}</td>
-      </tr>
-      <tr>
-        <td>Defendido</td>
-        <td>{{ $profile->date->defended }}</td>
-      </tr>
-      <tr>
-        <td>Abandonado</td>
-        <td>{{ $profile->date->abandoned }}</td>
-      </tr>
-   
-
-    </table>
+      <div></div>  
+    </header>
+      <h3>Fechas establecidas en cada fase de la fecha de un perfil</h3>
+      <table>
+        <thead>
+          <tr>
+            <th class="states">Estado del Perfil</th>
+            <th class="date">Fecha</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="states">Iniciado</td>
+            <td class="date">{{ $profile->date->initiated }}</td>
+          </tr>
+          <tr>
+            <td class="states">Aprobado</td>
+            <td class="date">{{ $profile->date->approved }}</td>
+          </tr>
+          <tr>
+            <td class="states">Asigando con Tribunales</td>
+            <td class="date">{{ $profile->date->assigned }}</td>
+          </tr>
+          <tr>
+            <td class="states">Defendido</td>
+            <td class="date">{{ $profile->date->defended }}</td>
+          </tr>
+          <tr>
+            <td class="states">Finalizado</td>
+            <td class="date">{{ $profile->date->finalized }}</td>
+          </tr>
+          <tr>
+            <td class="states">Abandonado</td>
+            <td class="date">{{ $profile->date->abandoned }}</td>
+          </tr>
+        </tbody>
+      </table>
   </body>
 </html>
