9ddf97fff092c786727f05504b10510e8c4105ea
Misael Javier Zurita Canaviri <mija_zuca@hotmail.es>
2018-05-21 21:35:17 -0400

    reporte pdf

--- resources/views/profile/profile_report.blade.php
+++ resources/views/profile/profile_report.blade.php
@@ -7,7 +7,6 @@
   </head>
   <body>
     <h1>{{ $profile->title }}</h1>
-<<<<<<< HEAD
     <header>
       
       <div>  
@@ -67,58 +66,5 @@
           </tr>
         </tbody>
       </table>
-=======
-    <div >
-      @foreach($profile->students as $key => $student)
-        <div>TESISTA: {{ $student->name }} {{ $student->last_name_father }} {{ $student->last_name_mother }}</div>
-        <div>CARRERA: {{ $student->career }}</div>
-      @endforeach
-    </div>
-    <br>
-    <div>
-        @foreach($profile->tutors as $key => $tutor)
-          <div>TUTOR: {{ $tutor->name }} {{ $tutor->last_name_father }} {{ $tutor->last_name_mother }}</div>
-      @endforeach
-    </div>
-    <h2>TRIBUNALES:</h2>
-    <div>
-      @foreach($profile->courts as $key => $court)
-        <div>TRIBUNAL: {{ $court->name }} {{ $court->last_name_father }} {{ $court->last_name_mother }}</div>
-      @endforeach
-    </div>
-    <h4>Fechas establecidas en cada fase de la defensa del perfil</h3>
-
-    <table style="width:100%">
-      <tr>
-        <th>Estado del Perfil</th>
-        <th>Fecha</th>
-      </tr>
-      <tr>
-        <td>Iniciado</td>
-        <td>{{ $profile->date->initiated }}</td>
-      </tr>
-      <tr>
-        <td>Aprobado</td>
-        <td>{{ $profile->date->approved }}</td>
-      </tr>
-      <tr>
-        <td>Asigando</td>
-        <td>{{ $profile->date->assigned }}</td>
-      </tr>
-      <tr>
-        <td>Finalizado</td>
-        <td>{{ $profile->date->finalized }}</td>
-      </tr>
-      <tr>
-        <td>Defendido</td>
-        <td>{{ $profile->date->defended }}</td>
-      </tr>
-      <tr>
-        <td>Abandonado</td>
-        <td>{{ $profile->date->abandoned }}</td>
-      </tr>
-
-    </table>
->>>>>>> b20c9b7a63f5c2c0adcde1c4f13669a27754c4b5
   </body>
 </html>
