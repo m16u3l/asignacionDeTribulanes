edda842b5873b194260bf1ea5dc089bb8937b772
Oscar Antezana <caballerodelaweb@gmail.com>
2018-05-21 23:27:32 -0400

    correccion error en vista de cambio de profesional

--- resources/views/profile/list_profile_assigned.blade.php
+++ resources/views/profile/list_profile_assigned.blade.php
@@ -59,7 +59,7 @@
                   <a id="boton_finalizar_tribunal" href="#" class="btn btn-rounded btn-info mb-2" data-toggle="modal" data-target="#finalize_profile_modal"
                     data-finalize_profile="{{ $profile->id }}"><i class="fa fa-check"></i></a>
                   
-                  <a id="boton_modificar_tribunal" href="{{ route ('solicitud_rununcia',[$profile->id])}}" class="btn btn-rounded btn-info"><i class="fa fa-times"></i></a>
+                  <a id="boton_modificar_tribunal" href="{{ route ('solicitud_rununcia',[$profile->id])}}" class="btn btn-rounded btn-info"><i class="fa fa-exchange"></i></a>
                 </div>
               </div>
             </div>
